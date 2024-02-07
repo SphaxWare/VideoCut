1dfa42c2ee38d4d79908bd0dc6f06d2a2137edc6
SphaxWare <134861101+SphaxWare@users.noreply.github.com>
2024-02-07 22:41:50 +0100

    Create MultiToks.py

---
+++ MultiToks.py
@@ -0,0 +1,43 @@
+"""Convert Multiple Videos to tiktok format"""
+from moviepy.editor import VideoFileClip
+from datetime import datetime
+import os
+
+def convert_timestamp_to_seconds(timestamp):
+    time_obj = datetime.strptime(timestamp, '%H:%M:%S')
+    return time_obj.hour * 3600 + time_obj.minute * 60 + time_obj.second
+
+def trim_video(input_video, start_timestamp, end_timestamp, output_video):
+    clip = VideoFileClip(input_video)
+
+    if start_timestamp is not None:
+        start_time = convert_timestamp_to_seconds(start_timestamp)
+    else:
+        start_time = 0
+
+    if end_timestamp is not None:
+        end_time = convert_timestamp_to_seconds(end_timestamp)
+    else:
+        end_time = clip.duration
+
+    clip = clip.subclip(start_time, end_time)
+
+    # Crop to 9:16 aspect ratio
+    cropped_clip = clip.crop(x1=(clip.w - clip.h * 9 / 16) / 2,
+                             x2=clip.w - (clip.w - clip.h * 9 / 16) / 2,
+                             y1=0, y2=clip.h)
+
+    # Set bitrate to 10 mb/s
+    cropped_clip.write_videofile(output_video, codec="libx264", audio_codec="aac", bitrate="10M")
+
+if __name__ == "__main__":
+    input_videos = input("Enter the paths to the input video files (separated by comma): ").split(',')
+    input_videos = [video.strip().replace('"', '') for video in input_videos]
+    
+    for input_video in input_videos:
+        start_timestamp = input("Enter the start timestamp (hh:mm:ss): ")
+        end_timestamp = input("Enter the end timestamp (hh:mm:ss, press Enter for full video): ")
+        output_video = input(f"Enter the path for the output video file for {os.path.basename(input_video)}: ") + ".mp4"
+
+        trim_video(input_video, start_timestamp or None, end_timestamp or None, output_video)
+      
